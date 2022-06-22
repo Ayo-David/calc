2982df467463dec15b3d4bdba508dc6ceb33f0cc
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2022-06-22 20:01:35 +0100

    output

---
+++ src/Output.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Output.css'
+
+const Output = ({ value }) => {
+    return (
+        <div class="output">
+            {value}
+        </div>
+    );
+}
+
+export default Output
